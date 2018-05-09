6a3fb4bed03e6ad2e925fc2dcd7ec1347e075dc5
Kyle Corsi <kyle.corsi@gmail.com>
2018-05-09 18:32:48 +0000

    Update AssemblyInfo.cs

--- Direct-Messaging-SDK-4.6.1 Unit Tests/Properties/AssemblyInfo.cs
+++ Direct-Messaging-SDK-4.6.1 Unit Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Direct-Messaging-SDK-4.6.1 Unit Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Direct-Messaging-SDK-4.6.1 Unit Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
 
 [assembly: Guid("8a59e6d0-e80f-49f2-a151-beae6ba09ad1")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
