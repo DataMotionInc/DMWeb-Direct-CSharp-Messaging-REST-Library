4ae23d0a4a9838920c24fd7f2e2c900aa9b88134
Kyle Corsi <kyle.corsi@gmail.com>
2018-05-09 18:36:22 +0000

    Add new file

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+ 
+[assembly: AssemblyCompany("DataMotion")]
+[assembly: AssemblyProduct("DataMotion Web Library")]
+[assembly: AssemblyCopyright("DataMotion 2018")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+ 
+[assembly: ComVisible(false)]
+ 
+[assembly: AssemblyVersion("1.2.3.4")] //Will be replaced
+//[assembly: AssemblyFileVersion("1.0.0.0")] //Will be replaced
+ 
+[assembly: NeutralResourcesLanguage("en-US")]
