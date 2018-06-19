a1b912e0046d91b74e82cb3410b0224f9b58a21c
Christopher Ly <ChristopherL@datamotion.com>
2018-06-19 15:11:45 -0400

    Added AssemblyInfo.cs for 3.5

---
+++ Direct-Messaging-SDK-3.5 Unit Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Direct-Messaging-SDK-3.5 Unit Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: Guid("d4719ce6-5236-4b67-8856-bd2d4e93404d")]
